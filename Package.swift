fca9d5abaa712ad3e17c6dd0e9d386295e80723a
Paolo Leonardi <pao.leonardi@gmail.com>
2021-08-25 15:56:51 +0200

    update Package and link library with project

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "JustTweak",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12),
+        .tvOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -20,7 +26,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "JustTweak",
-            dependencies: []),
+            resources: [
+                .process("Assets")]
+        ),
         .testTarget(
             name: "JustTweakTests",
             dependencies: ["JustTweak"]),
