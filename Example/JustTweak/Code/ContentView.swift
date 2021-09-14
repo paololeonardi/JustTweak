f7c8fdec365bfc2981117725a1654662ca05f2e0
Paolo Leonardi <pao.leonardi@gmail.com>
2021-09-14 02:45:50 +0100

    remove tap gestures

--- Example/JustTweak/Code/ContentView.swift
+++ Example/JustTweak/Code/ContentView.swift
@@ -14,50 +14,71 @@ struct ContentView: View {
 
     @ObservedObject var viewModel: ContentViewModel
 
+    @ViewBuilder
+    private var viewBackground: some View {
+        if backgroundColor != nil {
+            Rectangle()
+                .foregroundColor(backgroundColor)
+                .ignoresSafeArea()
+        }
+    }
+
     var body: some View {
         NavigationView {
             GeometryReader { geometry in
-                VStack {
-                    VStack {
+                ZStack {
+                    viewBackground
+                    ScrollView {
                         VStack {
-                            if viewModel.tweakAccessor.canShowRedView {
-                                let alpha = viewModel.tweakAccessor.redViewAlpha
-                                Rectangle()
-                                    .foregroundColor(.red.opacity(alpha))
+                            VStack {
+                                VStack {
+                                    if viewModel.tweakAccessor.canShowRedView {
+                                        let alpha = viewModel.tweakAccessor.redViewAlpha
+                                        Rectangle()
+                                            .foregroundColor(.red.opacity(alpha))
+                                    }
+                                    if viewModel.tweakAccessor.canShowYellowView {
+                                        Rectangle()
+                                            .foregroundColor(.yellow)
+                                    }
+                                    if viewModel.tweakAccessor.canShowGreenView {
+                                        Rectangle()
+                                            .foregroundColor(.green)
+                                    }
+                                }
+                                .frame(width: geometry.size.width, height: geometry.size.height / 2)
+                                Text(viewModel.tweakAccessor.labelText)
+                                    .font(.headline)
+                                    .padding()
                             }
-                            if viewModel.tweakAccessor.canShowYellowView {
-                                Rectangle()
-                                    .foregroundColor(.yellow)
-                            }
-                            if viewModel.tweakAccessor.canShowGreenView {
-                                Rectangle()
-                                    .foregroundColor(.green)
-                            }
-                        }
-                        .frame(width: geometry.size.width, height: geometry.size.height / 2)
-                        Text(viewModel.tweakAccessor.labelText)
-                            .font(.headline)
-                            .padding()
-                        Rectangle()
-                            .border(Color.blue, width: 5)
-                            .foregroundColor(backgroundColor)
-                    }
-                    #if !os(tvOS)
-//                    .gesture(doubleTapGesture)
-//                    .simultaneousGesture(singleTapGesture)
-                    #endif
 
-                    VStack(spacing: 16) {
-                        #if os(iOS)
-                        Button("Change configuration (present)") {
-                            presentTweakView.toggle()
-                        }
-                        #endif
-                        NavigationLink(destination: viewModel.tweakView) {
-                            Text("Change configuration (push)")
+                            VStack(spacing: 16) {
+                                Button("Set meaning of life") {
+                                    viewModel.setMeaningOfLife()
+                                    showingAlert = true
+                                }
+
+                                Button("Change background color") {
+                                    backgroundColor = viewModel.randomColor()
+                                }
+                                .disabled(!viewModel.tweakAccessor.isTapGestureToChangeColorEnabled)
+                                .background(backgroundColor)
+                            }
+                            .padding()
+
+                            VStack(spacing: 16) {
+                                #if os(iOS)
+                                Button("Change configuration (present)") {
+                                    presentTweakView.toggle()
+                                }
+                                #endif
+                                NavigationLink(destination: viewModel.tweakView) {
+                                    Text("Change configuration (push)")
+                                }
+                            }
+                            .padding()
                         }
                     }
-                    .padding()
                 }
             }
             .navigationTitle("JustTweak")
@@ -89,24 +110,5 @@ struct ContentView: View {
             )
         }
     }
-
-    #if !os(tvOS)
-    var singleTapGesture: some Gesture {
-        TapGesture()
-            .onEnded {
-                if viewModel.tweakAccessor.isTapGestureToChangeColorEnabled {
-                    backgroundColor = viewModel.randomColor()
-                }
-            }
-    }
-
-    var doubleTapGesture: some Gesture {
-        TapGesture(count: 2)
-            .onEnded {
-                viewModel.setMeaningOfLife()
-                showingAlert = true
-            }
-    }
-    #endif
 
 }
